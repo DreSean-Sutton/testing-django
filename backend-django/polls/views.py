78b9f5eeb08c164fdaa848cae930529b74dbcb2e
DreSean-Sutton <dreseansutton@gmail.com>
2023-10-05 00:07:04 -0700

    WIP Part two

--- backend-django/polls/views.py
+++ backend-django/polls/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def another_test(request):
+    return HttpResponse("Hello, world. This is another test!")
